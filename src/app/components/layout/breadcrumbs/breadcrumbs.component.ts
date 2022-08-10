1098481ee7cb8e9d52ab4f810ab35e358a5628d4
Adriana Franklin Sanchez <adriana.franklin@sofka.com.co>
2022-08-10 16:22:47 -0500

    [DELETE] coments in breadcrumbs

--- src/app/components/layout/breadcrumbs/breadcrumbs.component.ts
+++ src/app/components/layout/breadcrumbs/breadcrumbs.component.ts
@@ -2,11 +2,9 @@ import { IBreadcrumb } from './../../../common/models/breadcrumb.interface';
 import { Component, OnInit } from '@angular/core';
 import { FacadeService } from 'src/app/services/facade.service';
 
-import { NbIconLibraries } from '@nebular/theme';
 import { gameService } from '../../../services/game.service';
-import { distinctUntilChanged, filter, map, tap } from 'rxjs';
-import { ActivatedRoute, ActivationEnd, NavigationEnd, Router } from '@angular/router';
-import { CloseScrollStrategy } from '@angular/cdk/overlay';
+import { filter, map } from 'rxjs';
+import { ActivationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -21,13 +19,10 @@ export class BreadcrumbsComponent implements OnInit {
   constructor(
     private gameService: gameService,
     private facade: FacadeService,
-    private iconLibraries: NbIconLibraries,
+
     private router: Router,
-    private activatedRoute: ActivatedRoute,) {
+) {
 
-      // this.iconLibraries.registerFontPack('fa', { iconClassPrefix: 'fa' });
-
-      // this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
       this.router.events.pipe(
         filter((event): event is ActivationEnd => event instanceof ActivationEnd),
         filter((event: ActivationEnd) => event.snapshot.firstChild == null ),
@@ -39,12 +34,7 @@ export class BreadcrumbsComponent implements OnInit {
           console.log(this.breadcrumb1)})
   }
   ngOnInit(): void {
-  //   this.router.events.pipe(
-  //     filter((event): event is NavigationEnd => event instanceof NavigationEnd),
-  //     distinctUntilChanged(),
-  // ).subscribe(() => {
-  //     this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
-  // })
+
 
   }
 
@@ -59,40 +49,6 @@ export class BreadcrumbsComponent implements OnInit {
   navigateToPodium() {
     this.facade.navigateToPodium()
   }
-
-  buildBreadCrumb(route: ActivatedRoute, url: string = '', breadcrumbs: IBreadcrumb[] = []): IBreadcrumb[] {
-    //If no routeConfig is avalailable we are on the root path
-    let label = route.routeConfig && route.routeConfig.data ? route.routeConfig.data['breadcrumb'] : '';
-    let path = route.routeConfig && route.routeConfig.data ? route.routeConfig.path : '';
-
-    // If the route is dynamic route such as ':id', remove it
-    const lastRoutePart = path?.split('/').pop();
-    const isDynamicRoute = lastRoutePart?.startsWith(':');
-    if(isDynamicRoute && !!route.snapshot) {
-      const paramName = lastRoutePart?.split(':')[1];
-      path = path?.replace(lastRoutePart!, route.snapshot.params[paramName!]);
-      label = route.snapshot.params[paramName!];
-    }
-
-    //In the routeConfig the complete path is not available,
-    //so we rebuild it each time
-    const nextUrl = path ? `${url}/${path}` : url;
-
-    const breadcrumb: IBreadcrumb = {
-        label: label,
-        url: nextUrl,
-    };
-    // Only adding route with non-empty label
-    const newBreadcrumbs = breadcrumb.label ? [ ...breadcrumbs, breadcrumb ] : [ ...breadcrumbs];
-    if (route.firstChild) {
-        //If we are not on our current path yet,
-        //there will be more children to look after, to build our breadcumb
-        return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
-    }
-    return newBreadcrumbs;
-  }
-
-// ---fernando herrera
 
 
 
